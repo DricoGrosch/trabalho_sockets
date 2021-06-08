f081441392341be339b7959a172ee2acd1c2888f
Adrian Grosch <adrian.grosch@singular.inf.br>
2021-06-07 23:51:08 -0300

    jars

--- server/src/main.java
+++ server/src/main.java
@@ -9,7 +9,7 @@ public class main {
     public static void main(String[] args) throws IOException, ScriptException {
         Scanner scan = new Scanner(System.in);
         System.out.println("Wish port would you like to star the server ?");
-//        Server server = new Server(scan.nextInt());
-        Server server = new Server(8000);
+        Server server = new Server(scan.nextInt());
+//        Server server = new Server(8000);
     }
 }
